50ce3e9fa39c72ed7f644377c8138725c4bff962
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-24 16:20:41 -0400

    added slight edit to auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,35 +5,36 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  // function for authenticated routes
   authMiddleware: function({ req }) {
-  // allows token to be sent via req.body, req.query, or headers
-  let token = req.body.token || req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-  // separate "Bearer" from "<tokenvalue>"
-  if (req.headers.authorization) {
-    token = token
-      .split(' ')
-      .pop()
-      .trim();
-  }
+    // separate "Bearer" from "<tokenvalue>"
+    if (req.headers.authorization) {
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
+    }
 
-  // if no token, return request object as is
-  if (!token) {
+    // if no token, return request object as is
+    if (!token) {
+      return req;
+    }
+
+    try {
+      // decode and attach user data to request object
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    // return updated request object
     return req;
-  }
-
-  try {
-    // decode and attach user data to request object
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    req.user = data;
-  } catch {
-    console.log('Invalid token');
-  }
-
-  // return updated request object
-  return req;
-},
+  },
+  // function to sign tokens
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
